bf72720310a4b9d61142fb06d58ff1252e9915dc
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2018-06-15 19:08:26 +0900

    Add NGINX_UPSTREAM_PROTOCOL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '$$NGINX_SERVER_NAME $$NGINX_SERVER_PORT $$NGINX_UPSTREAM_HOST $$NGINX_UPSTREAM_PORT' < /etc/nginx/proxy.conf.template > /etc/nginx/conf.d/proxy.conf
+envsubst '$$NGINX_SERVER_NAME $$NGINX_SERVER_PORT $$NGINX_UPSTREAM_PROTOCOL $$NGINX_UPSTREAM_HOST $$NGINX_UPSTREAM_PORT' < /etc/nginx/proxy.conf.template > /etc/nginx/conf.d/proxy.conf
 
 exec "$@"
